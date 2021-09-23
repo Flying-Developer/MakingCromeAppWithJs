96b62ff42cdf2306fc3599caa697c73f4b9e9d88
Flown Developer <kopasd99@gmail.com>
2021-09-23 12:59:41 +0900

    OnlyOne

---
+++ Js/background.js
@@ -0,0 +1,44 @@
+const images = ["3.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+document.body.background = `Img/${chosenImage}`;
+
+/*
+const images = [
+  "src/img/007.png",
+  "src/img/008.jpg",
+  "https://images.unsplash.com/reserve/aOcWqRTfQ12uwr3wWevA_14401305508_804b300054_o.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80",
+  "https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80",
+  "https://images.unsplash.com/photo-1487700160041-babef9c3cb55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1035&q=80",
+  "https://images.unsplash.com/photo-1504548840739-580b10ae7715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+//랜덤 이미지 선택
+const body = document.querySelector("body");
+const bgImg = document.createElement("img");
+
+bgImg.src = `${chosenImage}`;
+
+body.style.background = `url(${bgImg.src})`;
+body.style.backgroundRepeat = "no-repeat";
+body.style.backgroundSize = "cover";
+
+const images = [
+  "https://images.unsplash.com/reserve/aOcWqRTfQ12uwr3wWevA_14401305508_804b300054_o.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80",
+  "https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80",
+  "https://images.unsplash.com/photo-1487700160041-babef9c3cb55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1035&q=80",
+  "https://images.unsplash.com/photo-1504548840739-580b10ae7715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+  ];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const body = document.querySelector("body");
+const bgImg = document.createElement("img");
+
+bgImg.src = `${chosenImage}`;
+
+body.style.background = `url(${bgImg.src})`;
+body.style.backgroundRepeat = "no-repeat";
+body.style.backgroundSize = "cover";
+*/
